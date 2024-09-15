83f3cb70057c6beaa63944ffc727477919c628f9
Mostafa Asaad <ma4190243@gmail.com>
2024-09-16 01:15:55 +0300

    Some changes for project's configurations

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,14 +1,17 @@
 import express, { Application } from 'express';
 import { dbConnection } from './database/database'
+import { StudentModel } from './Models/student.model'
+import studentRoutes  from './Routes/student.routes'
 
 const app:Application = express();
 const port = process.env.APP_PORT || 4000;
 
-app.get('/', (req, res) => {   
+app.get('/', async (req, res) => {
     res.json({
         project: 'Full-Stack, Long-Term Simple School Platform'
     });
 });
+app.use('/students', studentRoutes);
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
