f9becabfb8bd05ead0b0b5fdef192ff80276a116
Mostafa Asaad <ma4190243@gmail.com>
2024-09-20 17:37:22 +0300

    Added create and delete student controllers

--- Back-end/build/Controllers/student.controller.js
+++ Back-end/build/Controllers/student.controller.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateStudentInfo = exports.getStudentById = exports.getAllStudent = void 0;
+exports.deleteStudent = exports.createNewStudent = exports.getCreateView = exports.updateStudentInfo = exports.getStudentById = exports.getAllStudent = void 0;
 const student_model_1 = require("../Models/student.model");
 const getAllStudent = async (req, res) => {
     try {
@@ -31,3 +31,41 @@ exports.getStudentById = getStudentById;
 const updateStudentInfo = async (req, res) => {
 };
 exports.updateStudentInfo = updateStudentInfo;
+const getCreateView = (req, res) => {
+    res.status(200).send('Create a new student');
+};
+exports.getCreateView = getCreateView;
+const createNewStudent = async (req, res) => {
+    let studentInfo = {};
+    console.log(req.body);
+    studentInfo.id = req.body.id;
+    studentInfo.NID = req.body.Nid;
+    studentInfo.Name = req.body.Name;
+    studentInfo.phoneNumber = req.body.phoneNumber;
+    studentInfo.department = req.body.department;
+    studentInfo.dateOfBirth = req.body.dateOfBirth;
+    const result = await student_model_1.StudentModel.create(studentInfo);
+    if (result) {
+        res.status(201).send("Student Registered successfully");
+    }
+    else {
+        res.status(500).send("Student was not added");
+    }
+};
+exports.createNewStudent = createNewStudent;
+const deleteStudent = async (req, res) => {
+    try {
+        const studentId = req.params.id;
+        const result = await student_model_1.StudentModel.delete(studentId);
+        if (result === true) {
+            res.status(200).send("Student info was deleted");
+        }
+        else {
+            res.status(404).send("Student info does not exist");
+        }
+    }
+    catch (error) {
+        res.status(500);
+    }
+};
+exports.deleteStudent = deleteStudent;
