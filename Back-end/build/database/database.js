0041958adb404325050804cd0a61804c59569c0b
Mostafa Asaad <ma4190243@gmail.com>
2024-09-21 15:17:42 +0300

    Disabled multiple statements to avoid sql i

--- Back-end/build/database/database.js
+++ Back-end/build/database/database.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConnection = void 0;
-const mysql2_1 = __importDefault(require("mysql2"));
-exports.dbConnection = mysql2_1.default.createConnection({
+const promise_1 = __importDefault(require("mysql2/promise"));
+exports.dbConnection = promise_1.default.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    multipleStatements: false,
     database: process.env.DB_NAME
 });
