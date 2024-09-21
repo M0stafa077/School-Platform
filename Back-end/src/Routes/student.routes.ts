361910ef571e0b970812816842c9add03d34d811
Mostafa Asaad <ma4190243@gmail.com>
2024-09-21 13:54:39 +0300

    Refactoring the delete-student route

--- Back-end/src/Routes/student.routes.ts
+++ Back-end/src/Routes/student.routes.ts
@@ -1,19 +1,21 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import * as controllers from '../Controllers/student.controller'
 
 const studentRoutes = Router();
 
 studentRoutes.get('/', controllers.getAllStudent);
+
 studentRoutes.route('/create')
     .get(controllers.getCreateView)
     .post(controllers.createNewStudent);
+
 studentRoutes.route('/:id') 
     .get(controllers.getStudentById)
+    .delete(controllers.deleteStudent)
     .patch(controllers.updateStudentInfo);
 
-studentRoutes.get('/delete-student', (req, res)=> {
+studentRoutes.get('/delete-student', (req: Request, res: Response)=> {
     res.json({ message: "Delete a student" });
 });    // View
-studentRoutes.delete('/delete-student/:id', controllers.deleteStudent);
 
 export default studentRoutes;
