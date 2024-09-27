80d2a9d7989f8acc7987bba0c0844ab7d92c047b
Mostafa Asaad <ma4190243@gmail.com>
2024-09-27 18:30:38 +0300

    Linked swagger documentation

--- Back-end/build/index.js
+++ Back-end/build/index.js
@@ -3,10 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const student_routes_1 = __importDefault(require("./Routes/student.routes"));
+const yaml_1 = __importDefault(require("yaml"));
+const swaggerFilePath = path_1.default.join(__dirname, "../docs/swagger.yml");
+const swaggerFileContents = fs_1.default.readFileSync(swaggerFilePath, 'utf8');
+const swaggerDocument = yaml_1.default.parse(swaggerFileContents);
 const app = (0, express_1.default)();
 const port = process.env.APP_PORT || 4000;
+app.use(express_1.default.json());
+app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
 app.get('/', async (req, res) => {
     res.json({
         project: 'Full-Stack, Long-Term Simple School Platform'
--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,11 +1,21 @@
 import express, { Application } from 'express';
-import { dbConnection } from './database/database'
-import { StudentModel } from './Models/student.model'
 import studentRoutes  from './Routes/student.routes'
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import yml from 'yaml';
+
+const swaggerFileContents = fs.readFileSync(path.join(__dirname, "../docs/swagger.yml"), 'utf8');
+const swaggerDocument = yml.parse(swaggerFileContents);
 
 const app:Application = express();
 const port = process.env.APP_PORT || 4000;
 app.use(express.json());
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
 
 app.get('/', async (req, res) => {
     res.json({
