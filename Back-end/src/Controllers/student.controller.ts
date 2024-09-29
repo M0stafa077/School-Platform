de2f9b672361c4bd5c412d16beaeb747b2bf21fe
Mostafa Asaad <ma4190243@gmail.com>
2024-09-29 23:07:29 +0300

    prettier formatting '(

--- Back-end/src/Controllers/student.controller.ts
+++ Back-end/src/Controllers/student.controller.ts
@@ -3,38 +3,36 @@ import { student } from "../Types/student.type";
 import { StudentModel } from "../Models/student.model";
 
 export const getAllStudent = async (req: Request, res: Response) => {
-    try{
+    try {
         let students: student[] | Error = await StudentModel.getAll();
-        if(students instanceof Error){
+        if (students instanceof Error) {
             res.status(500).send("Internal server error");
-        }
-        else if(Array(students).length === 0){
+        } else if (Array(students).length === 0) {
             res.status(204).send("There's no student data");
-        }
-        else{
+        } else {
             res.json(students);
         }
-    }
-    catch(err){
-        console.log('Error retrieving all students');
+    } catch (err) {
+        console.log("Error retrieving all students");
         throw err;
     }
 };
-export const getStudentById = async(req: Request, res: Response) => {
-    try{
+export const getStudentById = async (req: Request, res: Response) => {
+    try {
         let studentInfo: student[] = await StudentModel.getById(req.params.id);
-        if(studentInfo.length == 0)
-            { throw new Error(); }
+        if (studentInfo.length == 0) {
+            throw new Error();
+        }
         res.json(studentInfo[0]);
-    }
-    catch(err) {
-        console.log('Error retrieving a students by an id');
-        res.status(404)
-            .json({ message: `No Student with ID ${req.params.id}` });
+    } catch (err) {
+        console.log("Error retrieving a students by an id");
+        res.status(404).json({
+            message: `No Student with ID ${req.params.id}`,
+        });
     }
 };
-export const updateStudentInfo = async(req: Request, res: Response) => {
-    let studentInfo = {} as student|any;
+export const updateStudentInfo = async (req: Request, res: Response) => {
+    let studentInfo = {} as student | any;
     studentInfo.oldId = req.params.id;
     studentInfo.id = req.body.id;
     studentInfo.NID = req.body.Nid;
@@ -44,18 +42,16 @@ export const updateStudentInfo = async(req: Request, res: Response) => {
     studentInfo.dateOfBirth = req.body.dateOfBirth;
 
     const result = await StudentModel.updateInfo(studentInfo);
-    if(result === true){
+    if (result === true) {
         res.status(200).send("Student info is updated successfully");
-    }
-    else if(result === false){
+    } else if (result === false) {
         res.status(404).send("No student with this id");
-    }
-    else{
+    } else {
         res.status(500).send("Student info was not updated");
     }
 };
-export const updateStudentInfoBody = async(req: Request, res: Response) => {
-    let studentInfo = {} as student|any;
+export const updateStudentInfoBody = async (req: Request, res: Response) => {
+    let studentInfo = {} as student | any;
     studentInfo.id = req.body.id;
     studentInfo.oldId = studentInfo.id;
     studentInfo.NID = req.body.Nid;
@@ -65,17 +61,15 @@ export const updateStudentInfoBody = async(req: Request, res: Response) => {
     studentInfo.dateOfBirth = req.body.dateOfBirth;
 
     const result = await StudentModel.updateInfo(studentInfo);
-    if(result === true){
+    if (result === true) {
         res.status(200).send("Student info is updated successfully");
-    }
-    else if(result === false){
+    } else if (result === false) {
         res.status(404).send("No student with this id");
-    }
-    else{
+    } else {
         res.status(500).send("Student info was not updated");
     }
 };
-export const createNewStudent = async(req: Request, res: Response) => {
+export const createNewStudent = async (req: Request, res: Response) => {
     let studentInfo: student = {} as student;
     // console.log(req.body);
     studentInfo.id = req.body.id;
@@ -86,15 +80,16 @@ export const createNewStudent = async(req: Request, res: Response) => {
     studentInfo.dateOfBirth = req.body.dateOfBirth;
 
     StudentModel.create(studentInfo)
-        .then(result => {
+        .then((result) => {
             res.status(201).send("Student Registered successfully");
         })
-        .catch(error => {
-            if(error === false){
+        .catch((error) => {
+            if (error === false) {
                 res.status(409).send("Student already exists");
-            }
-            else{
-                res.status(500).send("Error adding the student info, try again later");
+            } else {
+                res.status(500).send(
+                    "Error adding the student info, try again later"
+                );
             }
         });
 };
@@ -103,14 +98,16 @@ export const deleteStudent = async (req: Request, res: Response) => {
         const studentId = req.params.id;
         const result = await StudentModel.delete(studentId);
         // console.log(result);
-        if(result === true){
+        if (result === true) {
             res.status(200).send("Student info was deleted");
-        }else if(result === false){
+        } else if (result === false) {
             res.status(404).send("Student info does not exist");
-        }else{
-            res.status(500).send("Error deleting the student info, try again later!");// error occured
+        } else {
+            res.status(500).send(
+                "Error deleting the student info, try again later!"
+            ); // error occured
         }
     } catch (error) {
         res.status(500);
     }
-}
+};
