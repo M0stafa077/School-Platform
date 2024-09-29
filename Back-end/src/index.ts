c91a7b6734481afe538bbd78ee140fdabc31588e
Mostafa Asaad <ma4190243@gmail.com>
2024-09-29 23:21:43 +0300

    Added grades router to the app + prettier formatting

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,30 +1,31 @@
-import express, { Application } from 'express';
-import studentRoutes  from './Routes/student.routes'
-import { teacherRoutes } from './Routes/teacher.routes';
-import swaggerUi from 'swagger-ui-express';
-import fs from 'fs';
-import path from 'path';
-import yml from 'yaml';
+import express, { Application } from "express";
+import studentRoutes from "./Routes/student.routes";
+import { teacherRoutes } from "./Routes/teacher.routes";
+import gradesRoutes from "./Routes/grades.routes";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+import yml from "yaml";
 
-const swaggerFileContents = fs.readFileSync(path.join(__dirname, "../docs/swagger.yml"), 'utf8');
+const swaggerFileContents = fs.readFileSync(
+    path.join(__dirname, "../docs/swagger.yml"),
+    "utf8"
+);
 const swaggerDocument = yml.parse(swaggerFileContents);
 
-const app:Application = express();
+const app: Application = express();
 const port = process.env.APP_PORT || 4000;
 app.use(express.json());
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
     res.json({
-        project: 'Full-Stack, Long-Term Simple School Platform'
+        project: "Full-Stack, Long-Term Simple School Platform",
     });
 });
-app.use('/students', studentRoutes);
+app.use("/students", studentRoutes);
 app.use("/teachers", teacherRoutes);
+app.use("/grades", gradesRoutes);
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
